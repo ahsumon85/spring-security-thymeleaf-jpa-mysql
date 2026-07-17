10b9f6ad3eb7e5a7992e6ddc1bef368bc10b91bd
ahsumon85 <habibsumoncse@gmail.com>
2026-07-17 22:27:15 +0600

    Entity name chnage

--- spring-boot-basic-security/src/main/java/com/springbooth/model/entity/User.java
+++ spring-boot-basic-security/src/main/java/com/springbooth/model/entity/User.java
@@ -16,7 +16,7 @@ import org.hibernate.validator.constraints.Length;
 import com.springbooth.model.dto.RoleDTO;
 
 @Entity
-@Table(name = "user")
+@Table(name = "\"user\"")
 public class User {
 
 	@Id
